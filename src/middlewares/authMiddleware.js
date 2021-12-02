9d7ec886e7e18f85165b0b22e5abc492c171dd51
Pablo Menichini <pablo.menichini@gmail.com>
2021-12-02 01:11:28 -0300

    fix user role bugs

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 module.exports = {
     isAdmin: (req, res, next) => {
-        if (req.session.userLogged && req.session.userLogged.category === 'admin') {
+        if (req.session.userLogged && req.session.userLogged.role_id == 1) {
             next();
         } else {
             res.redirect('/users/login');
